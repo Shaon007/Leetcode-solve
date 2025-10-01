d0f9d98e1e456998d2f69db8fd66398b71034d57
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2025-10-01 18:26:42 +0600

    pascal triangle printing only row value

---
+++ pascalTriangle2.js
@@ -0,0 +1,13 @@
+function getRow(rowIndex) {
+  let row = [1];
+
+  for (let i = 1; i <= rowIndex; i++) {
+    for (let j = i - 1; j >= 1; j--) {
+      row[j] = row[j] + row[j - 1];
+    }
+    row.push(1);
+  }
+
+  return row;
+}
+console.log(getRow(3));
