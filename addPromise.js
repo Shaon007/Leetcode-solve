9de55a90a6892b718f845f338e537cf32b081c05
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2025-10-09 00:24:11 +0600

    add two promises

---
+++ addPromise.js
@@ -0,0 +1,5 @@
+async function addTwoPromises(promise1, promise2) {
+  const [value1, value2] = await Promise.all([promise1, promise2]);
+  return value1 + value2;
+}
+// console.log(addTwoPromises());
