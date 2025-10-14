42d93a9ade46c634ef23bfedb3e90397357615bf
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2025-10-14 14:30:00 +0600

    Consistent String finding

---
+++ ConsistentString.js
@@ -0,0 +1,17 @@
+function countConsistentStrings(allowed, words) {
+  let count = 0;
+  for (i = 0; i < words.length; i++){
+    let word = words[i];
+    isConsistent = true;
+    for (j = 0; j < word.length; j++){
+      if (!allowed.includes(word[j])) {
+        isConsistent = false
+        break;
+      }
+    }
+    if (isConsistent) count++
+  }
+
+  return count;
+}
+console.log(countConsistentStrings("ab", ["ad", "bd", "aaab", "baa", "badab"]));
