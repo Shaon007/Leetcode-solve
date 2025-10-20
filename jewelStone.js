317d86f51f212045a783ad6d633c91aa8d63d326
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2025-10-20 14:55:40 +0600

    jewel count from stone

---
+++ jewelStone.js
@@ -0,0 +1,9 @@
+function numJewelsInStones(jewels, stones) {
+  let count = 0;
+  for (let stone of stones) {
+    if (jewels.includes(stone)) count++;
+  }
+  return count;
+}
+
+console.log(numJewelsInStones('aA','aAAbbabb'));
