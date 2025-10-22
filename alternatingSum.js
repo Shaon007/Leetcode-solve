b9a771e9c48c66fadbe8f725e2437e5ac7bfe5cb
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2025-10-23 00:08:40 +0600

    compute alternate sum

---
+++ alternatingSum.js
@@ -0,0 +1,9 @@
+function alternatingSum(nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++){
+    if (i % 2 == 0) sum += nums[i];
+    else sum -= nums[i];
+  }
+  return sum;
+}
+console.log(alternatingSum([1,3,5,7]));
